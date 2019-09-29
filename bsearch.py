029019aa2f6282c446674e22b7d1f8bbdea85100
Dimitri Walters <me@dimi.io>
2019-09-29 18:15:11 -0400

    Add diff variations of bsearch

--- bsearch.py
+++ bsearch.py
@@ -1,18 +1,36 @@
 #!/bin/python3
 
-def bin_search(a, e):
+# def bsearch(a, e):
+#     lo, hi = 0, len(a)-1
+#     while lo <= hi:
+#         mid = (hi+lo)//2
+#         if e < a[mid]: hi = mid - 1
+#         elif e > a[mid]: lo = mid + 1
+#         else: return mid
+#     return -1
+
+def bsearch(a, e): # return index to insert
+    if e < a[0]: return 0
     lo, hi = 0, len(a)-1
     while lo <= hi:
-        mid = int((hi+lo)/2)
+        mid = (hi+lo)//2
         if e < a[mid]: hi = mid - 1
         elif e > a[mid]: lo = mid + 1
-        else: return mid
-    if e > a[mid]: mid += 1
-    return mid
+        else: return mid + 1
+    return lo
 
-L = [1,2,3,4,5]
-print(bin_search(L, 1))
-print(bin_search(L, 2))
-print(bin_search(L, 3))
-print(bin_search(L, 4))
-print(bin_search(L, 5))
+def bisect(a, e):
+    lo, hi = 0, len(a)
+    while lo < hi:
+        mid = (hi+lo)//2
+        if e < a[mid]: hi = mid
+        else: lo = mid + 1
+    return lo
+
+# L = [1,3,5,7,9,10]
+L = [2,4,6,8]
+print(bsearch(L, 1), bisect(L, 1))
+print(bsearch(L, 3), bisect(L, 3))
+print(bsearch(L, 5), bisect(L, 5))
+print(bsearch(L, 7), bisect(L, 7))
+print(bsearch(L, 9), bisect(L, 9))
